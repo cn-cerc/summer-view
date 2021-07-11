a09cec579324a00581ebe9689ce47976926ec575
张建红 <sz9214e@qq.com>
2021-07-11 14:33:13 +0000

    !3 增加Permission管理机制

--- summer-ui/src/main/java/cn/cerc/ui/mvc/StartServices.java
+++ summer-ui/src/main/java/cn/cerc/ui/mvc/StartServices.java
@@ -1,6 +1,8 @@
 package cn.cerc.ui.mvc;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Set;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,64 +12,98 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cn.cerc.core.DataSet;
+import cn.cerc.core.ISession;
 import cn.cerc.mis.core.Application;
 import cn.cerc.mis.core.BasicHandle;
+import cn.cerc.mis.core.DataValidateException;
 import cn.cerc.mis.core.IService;
+import cn.cerc.mis.core.Permission;
 import cn.cerc.mis.core.RequestData;
 import cn.cerc.mis.core.ServiceState;
 
 public class StartServices extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(StartServices.class);
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		this.doPost(request, response);
-	}
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doPost(request, response);
+    }
 
-	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String uri = req.getRequestURI();
-		log.debug(uri);
-		req.setCharacterEncoding("UTF-8");
-		RequestData param = new RequestData(req);
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String uri = req.getRequestURI();
+        log.debug(uri);
+        req.setCharacterEncoding("UTF-8");
+        RequestData param = new RequestData(req);
 
-		String token = param.getToken();
-		log.debug("DataIn Param==={}", param.getParam());
-		resp.setContentType("text/html;charset=UTF-8");
+        String token = param.getToken();
+        log.debug("DataIn Param==={}", param.getParam());
+        resp.setContentType("text/html;charset=UTF-8");
 
-		DataSet dataOut = new DataSet();
-		String sid = req.getSession().getId();
-		if (param.getService() == null) {
-			dataOut.setMessage("class is null.");
-			resp.getWriter().write(dataOut.toString());
-			return;
-		}
-		// 执行指定函数
-		log.debug(param.getService());
-		try (BasicHandle handle = new BasicHandle()) {
-			handle.getSession().setProperty(Application.SessionId, sid);
-			handle.getSession().loadToken(token);
-			IService bean = Application.getService(handle, param.getService());
-			if (bean == null) {
-				dataOut.setMessage(String.format("service(%s) is null.", param.getService()));
-				resp.getWriter().write(dataOut.toString());
-				return;
-			}
-			if (!bean.allowGuestUser(handle)) {
-				dataOut.setState(ServiceState.ACCESS_DISABLED);
-				dataOut.setMessage("请您先登入系统");
-				resp.getWriter().write(dataOut.toString());
-				return;
-			}
-			DataSet dataIn = new DataSet(param.getParam());
-			dataOut = bean.execute(dataIn);
-			resp.getWriter().write(dataOut.toString());
-		} catch (Exception e) {
-			Throwable err = e.getCause() != null ? e.getCause() : e;
-			log.error(err.getMessage(), err);
-			dataOut.setState(ServiceState.ERROR).setMessage(err.getMessage());
-			resp.getWriter().write(dataOut.toString());
-		}
-	}
+        DataSet dataOut = new DataSet();
+        String sid = req.getSession().getId();
+        if (param.getService() == null) {
+            dataOut.setMessage("class is null.");
+            resp.getWriter().write(dataOut.toString());
+            return;
+        }
+        // 执行指定函数
+        log.debug(param.getService());
+        try (BasicHandle handle = new BasicHandle()) {
+            handle.getSession().setProperty(Application.SessionId, sid);
+            handle.getSession().loadToken(token);
+            IService bean = null;
+            try {
+                bean = Application.getService(handle, param.getService());
+                if (bean == null) {
+                    dataOut.setMessage(String.format("service(%s) is null.", param.getService()))
+                            .setState(ServiceState.NOT_FIND_SERVICE);
+                    resp.getWriter().write(dataOut.toString());
+                    return;
+                }
+            } catch (ClassNotFoundException e) {
+                dataOut.setMessage(e.getMessage()).setState(ServiceState.NOT_FIND_SERVICE);
+                resp.getWriter().write(dataOut.toString());
+                return;
+            }
+            String permission = Permission.USERS;
+            for (Annotation item : bean.getClass().getDeclaredAnnotations()) {
+                if (item instanceof Permission)
+                    permission = ((Permission) item).value();
+            }
+            if (!Permission.GUEST.equals(permission)) {
+                ISession sess = handle.getSession();
+                if (!(sess != null && sess.logon())) {
+                    dataOut.setMessage("请您先登入系统").setState(ServiceState.ACCESS_DISABLED);
+                    resp.getWriter().write(dataOut.toString());
+                    return;
+                }
+                if (!Permission.USERS.equals(permission)) {
+                    Set<String> permissions = handle.getSession().getPermissions();
+                    if ((permissions != null) && (!permissions.contains(Permission.ADMIN))) {
+                        if (!permissions.contains(permission)) {
+                            dataOut.setMessage("您的无权执行当前服务").setState(ServiceState.ACCESS_DISABLED);
+                            resp.getWriter().write(dataOut.toString());
+                            return;
+                        }
+                    }
+                }
+            }
+            DataSet dataIn = new DataSet(param.getParam());
+            try {
+                dataOut = bean.execute(handle, dataIn);
+            } catch (DataValidateException e) {
+                dataOut.setMessage(e.getMessage());
+            }
+            if (dataOut == null)
+                dataOut = new DataSet().setMessage("service return empty");
+            resp.getWriter().write(dataOut.toString());
+        } catch (Exception e) {
+            Throwable err = e.getCause() != null ? e.getCause() : e;
+            log.error(err.getMessage(), err);
+            dataOut.setState(ServiceState.ERROR).setMessage(err.getMessage());
+            resp.getWriter().write(dataOut.toString());
+        }
+    }
 }
