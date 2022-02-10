6d87ca5516cf03e58bb2516dc784b8b9a3802798
韦善贵 <873261248@qq.com>
2022-02-10 12:33:37 +0000

    !56 IMenuBar 增加buildMenusBar方法定义

--- summer-ui/src/main/java/cn/cerc/ui/mvc/IMenuBar.java
+++ summer-ui/src/main/java/cn/cerc/ui/mvc/IMenuBar.java
@@ -1,5 +1,6 @@
 package cn.cerc.ui.mvc;
 
+import cn.cerc.mis.core.AbstractForm;
 import cn.cerc.mis.core.IForm;
 import cn.cerc.ui.core.UrlRecord;
 
@@ -8,4 +9,7 @@ import java.util.List;
 public interface IMenuBar {
     // 登记菜单栏菜单项
     int enrollMenu(IForm form, List<UrlRecord> menus);
+
+    // 购物车绑定菜单
+    String buildMenusBar(AbstractForm form);
 }
