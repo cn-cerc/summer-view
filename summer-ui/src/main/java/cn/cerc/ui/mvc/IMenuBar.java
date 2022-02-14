b367fd6c27b31a660fae0c12e364eb243923904d
韦善贵 <873261248@qq.com>
2022-02-14 11:41:29 +0000

    !57 IMenuBar 获得购物车名称 * IMenuBar 获得购物车名称

--- summer-ui/src/main/java/cn/cerc/ui/mvc/IMenuBar.java
+++ summer-ui/src/main/java/cn/cerc/ui/mvc/IMenuBar.java
@@ -1,10 +1,11 @@
 package cn.cerc.ui.mvc;
 
+import java.util.List;
+
+import cn.cerc.db.core.IHandle;
 import cn.cerc.mis.core.AbstractForm;
 import cn.cerc.mis.core.IForm;
 import cn.cerc.ui.core.UrlRecord;
-
-import java.util.List;
 
 public interface IMenuBar {
     // 登记菜单栏菜单项
@@ -12,4 +13,7 @@ public interface IMenuBar {
 
     // 购物车绑定菜单
     String buildMenusBar(AbstractForm form);
+
+    // 购物车名称
+    String buildShopName(IHandle handle, String tbType, boolean isPhone);
 }
