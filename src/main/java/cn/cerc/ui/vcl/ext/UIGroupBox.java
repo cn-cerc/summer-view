c532d8dfe10b5364e4fba8c46adf96c847cf3136
张建红 <sz9214e@qq.com>
2022-06-15 07:54:25 +0000

    !66 于UIGroupBox增加title属性，可配合FormEdit等使用

--- src/main/java/cn/cerc/ui/vcl/ext/UIGroupBox.java
+++ src/main/java/cn/cerc/ui/vcl/ext/UIGroupBox.java
@@ -4,18 +4,33 @@ import cn.cerc.mis.core.HtmlWriter;
 import cn.cerc.ui.core.UIComponent;
 
 public class UIGroupBox extends UIComponent {
+    private String title;
 
-    public UIGroupBox(UIComponent content) {
-        super(content);
+    public UIGroupBox(UIComponent owner) {
+        super(owner);
+        this.setRootLabel("div");
+        this.writeProperty("role", "group");
+    }
+
+    public UIGroupBox(UIComponent owner, String title) {
+        this(owner);
+        this.title = title;
     }
 
     @Override
-    public void output(HtmlWriter html) {
-        html.print("<div role='group'");
-        super.outputPropertys(html);
-        html.println(">");
-        super.output(html);
-        html.println("</div>");
+    public void beginOutput(HtmlWriter html) {
+        super.beginOutput(html);
+        if (this.title != null)
+            html.println("<legend>%s</legend>", this.title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void endOutput(HtmlWriter html) {
+        super.endOutput(html);
     }
 
 }
