c98f98ccb92420e9fa0563813b87d13ddca8ea2f
张弓 <sz9214e@qq.com>
2022-06-15 15:29:44 +0800

    改进UIGroupBox，增加title属性

--- src/main/java/cn/cerc/ui/vcl/ext/UIGroupBox.java
+++ src/main/java/cn/cerc/ui/vcl/ext/UIGroupBox.java
@@ -4,18 +4,36 @@ import cn.cerc.mis.core.HtmlWriter;
 import cn.cerc.ui.core.UIComponent;
 
 public class UIGroupBox extends UIComponent {
+    private String title;
 
-    public UIGroupBox(UIComponent content) {
-        super(content);
+    public UIGroupBox(UIComponent owner) {
+        super(owner);
+        this.setRootLabel("div");
+        this.writeProperty("role", "group");
+    }
+
+    public UIGroupBox(UIComponent owner, String title) {
+        super(owner);
+        this.writeProperty("role", "group");
+        this.title = title;
     }
 
     @Override
-    public void output(HtmlWriter html) {
-        html.print("<div role='group'");
-        super.outputPropertys(html);
-        html.println(">");
-        super.output(html);
-        html.println("</div>");
+    public void beginOutput(HtmlWriter html) {
+        super.beginOutput(html);
+        if (this.title != null)
+            html.println("<fieldset><legend>%s</legend>", this.title);
+    }
+
+    @Override
+    public void endOutput(HtmlWriter html) {
+        if (this.title != null)
+            html.println("</fieldset>");
+        super.endOutput(html);
+    }
+
+    public String getTitle() {
+        return title;
     }
 
 }
