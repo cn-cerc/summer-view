367aa02f1eb858467344d6a7aec5540400ab0092
胡红昌 <2220393782@qq.com>
2022-07-07 14:48:35 +0800

    使用StaticFile管控静态资源文件

--- src/main/java/cn/cerc/ui/page/StaticFile.java
+++ src/main/java/cn/cerc/ui/page/StaticFile.java
@@ -3,23 +3,94 @@ package cn.cerc.ui.page;
 import cn.cerc.mis.core.Application;
 
 public final class StaticFile {
-    private final String fileName;
-    private final String device;
+    private String fileName;
+    private String device;
+    private StaticFileType fileType;
+    private String fileRoot;
+    private String filePath;
 
+    private static final int Level_1 = 1;
+    private static final int Level_2 = 1;
+    private static final int Level_3 = 1;
+
+    public enum StaticFileType {
+        icon(Level_1), productImage(Level_1), otherImage(Level_1), cssFile(Level_2), jsFile(Level_2);
+
+        private int version;
+
+        public int getVersion() {
+            return version;
+        }
+
+        StaticFileType(int version) {
+            this.version = version;
+        }
+
+    }
+
+    @Deprecated
     public StaticFile(String fileName, String device) {
         super();
         this.fileName = fileName;
         this.device = device;
     }
 
-    protected final String getFileName() {
+    public StaticFile(StaticFileType fileType, String fileName) {
+        super();
+        this.fileRoot = Application.getStaticPath();
+//        if (ServerConfig.isServerDevelop())
+//            this.fileRoot = "/public/";
+        this.fileName = fileName;
+        switch (fileType) {
+        case cssFile:
+            filePath = "";
+            break;
+        case otherImage:
+            filePath = "resources/images/";
+            break;
+        default:
+            filePath = "";
+        }
+        this.fileType = fileType;
+    }
+
+    public static String getCssFile(String fileName) {
+        return new StaticFile(StaticFileType.cssFile, fileName).toString();
+    }
+
+    public static String getImage(String fileName) {
+        String file = fileName;
+        if (!fileName.toLowerCase().startsWith("http"))
+            file = new StaticFile(StaticFileType.jsFile, fileName).toString();
+        return file;
+    }
+    
+    public static String getJsFile(String fileName) {
+        String file = fileName;
+        if (!fileName.toLowerCase().startsWith("http"))
+            file = new StaticFile(StaticFileType.jsFile, fileName).toString();
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        if (this.fileType.getVersion() > 0)
+            return String.format("%s%s%s?v=%d", this.fileRoot, this.filePath, this.fileName,
+                    this.fileType.getVersion());
+        else
+            return String.format("%s%s%s", this.fileRoot, this.filePath, this.fileName);
+    }
+
+    public String getFileName() {
         return fileName;
     }
 
-    protected final String getDevice() {
+    @Deprecated
+    protected String getDevice() {
         return device;
     }
 
+    @Deprecated
     public String getUrl(String version) {
         String staticPath = Application.getStaticPath();
         String[] args = fileName.split("\\.");
