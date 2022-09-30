b979ccbf2309b211e65737e0bc1a2229d621cc64
凌鹏焰 <3324357875@qq.com>
2022-09-30 00:49:43 +0000

    !85 checkBox组件增加返回值

--- src/main/java/cn/cerc/ui/vcl/ext/UICheckBox.java
+++ src/main/java/cn/cerc/ui/vcl/ext/UICheckBox.java
@@ -14,6 +14,7 @@ public class UICheckBox extends UIComponent {
     private String value;
     private String role;
     private boolean checked;
+    private boolean disabled;
 
     public UICheckBox() {
         this(null);
@@ -32,18 +33,16 @@ public class UICheckBox extends UIComponent {
         if (this.name != null) {
             html.print(" name='%s'", this.getName());
         }
-        if (type != null) {
+        if (type != null)
             html.print(" type=\"%s\"", type);
-        }
-        if (value != null) {
+        if (value != null)
             html.print(" value='%s'", this.value);
-        }
-        if (role != null) {
+        if (role != null)
             html.print(" role='%s'", this.role);
-        }
-        if (checked) {
+        if (checked)
             html.print(" checked='checked'");
-        }
+        if (disabled)
+            html.print(" disabled='disabled'");
         html.println("/>");
     }
 
@@ -51,39 +50,53 @@ public class UICheckBox extends UIComponent {
         return name;
     }
 
-    public void setName(String name) {
+    public UICheckBox setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public UICheckBox setType(String type) {
         this.type = type;
+        return this;
     }
 
     public String getValue() {
         return value;
     }
 
-    public void setValue(String value) {
+    public UICheckBox setValue(String value) {
         this.value = value;
+        return this;
     }
 
     public boolean isChecked() {
         return checked;
     }
 
-    public void setChecked(boolean checked) {
+    public UICheckBox setChecked(boolean checked) {
         this.checked = checked;
+        return this;
     }
 
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
+    public UICheckBox setRole(String role) {
         this.role = role;
+        return this;
     }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
 }
