f0e100072fa098adb16f277598dac26ca8f069bb
张弓 <sz9214e@qq.com>
2022-10-02 20:26:22 +0800

    update

--- src/main/java/cn/cerc/ui/grid/UIGridView.java
+++ src/main/java/cn/cerc/ui/grid/UIGridView.java
@@ -22,7 +22,7 @@ public class UIGridView extends UIComponent implements UIDataViewImpl, IGridStyl
     private boolean active;
     private boolean init;
     private UITr head;
-    private UIGridLine body;
+    private UIGridBody body;
 
     public UIGridView(UIComponent owner) {
         super(owner);
@@ -98,7 +98,7 @@ public class UIGridView extends UIComponent implements UIDataViewImpl, IGridStyl
             // 根据不同的设备显示
 
             UITr head = head();
-            UIGridLine body = body();
+            UIGridBody body = body();
             for (var meta : fields) {
                 String fieldName = meta.name() == null ? meta.code() : meta.name();
                 new UITh(head).setText(fieldName);
@@ -115,9 +115,9 @@ public class UIGridView extends UIComponent implements UIDataViewImpl, IGridStyl
         return this.head;
     }
 
-    public UIGridLine body() {
+    public UIGridBody body() {
         if (body == null)
-            this.body = new UIGridLine(this);
+            this.body = new UIGridBody(this);
         return this.body;
     }
 
