50e4af0a3c706517e18807ccc9121f5d071bf9b6
HUANG Rongjun <l1091462907@gmail.com>
2023-06-16 14:16:58 +0800

    Update ClientIPVerify.java

--- src/main/java/cn/cerc/ui/mvc/ipplus/ClientIPVerify.java
+++ src/main/java/cn/cerc/ui/mvc/ipplus/ClientIPVerify.java
@@ -38,44 +38,57 @@ public class ClientIPVerify {
         if (!file.exists())
             return true;
 
+        JsonNode record;
         try (AWReader awReader = new AWReader(file)) {
             InetAddress address = InetAddress.getByName(ip);
-            JsonNode record = awReader.get(address);
+            record = awReader.get(address);
             if (record == null) {
                 log.error("{} IP地址读取不到地址解析", ip);
                 return false;
             }
+        } catch (IpTypeException | IOException e) {
+            log.error(e.getMessage(), e);
+            return true;
+        }
 
-            // 检查大洲通行的白名单
-            if (record.has("continent")) {
-                String continent = record.get("continent").asText();
-                if ("保留IP".equals(continent))
-                    return true;
-                if (client.getContinentWhiteList().stream().noneMatch(continent::contains)) {
-                    log.info("非法大洲ip {} {}", ip, record);
-                    return false;
-                }
+        // 检查大洲通行的白名单
+        if (record.has("continent")) {
+            String continent = record.get("continent").asText();
+            if ("保留IP".equals(continent))
+                return true;
+            if (client.getContinentWhiteList().stream().noneMatch(continent::contains)) {
+                log.info("非法大洲ip {} {}", ip, record);
+                return false;
             }
-            // 检查国家通行的白名单
-            if (record.has("country")) {
-                String country = record.get("country").asText();
-                if (client.getCountryWhitelist().stream().noneMatch(country::contains)) {
-                    log.info("非法国家ip {} {}", ip, record);
-                    return false;
-                }
+        }
+        // 检查国家通行的白名单
+        if (record.has("country")) {
+            String country = record.get("country").asText();
+            if (client.getCountryWhitelist().stream().noneMatch(country::contains)) {
+                log.info("非法国家ip {} {}", ip, record);
+                return false;
             }
-            // 检查省份通行的黑名单
-            if (record.has("province")) {
-                String province = record.get("province").asText();
-                if (client.getProvinceBlacklist().stream().anyMatch(province::contains)) {
-                    log.info("非法省份ip {} {}", ip, record);
-                    return false;
-                }
+        }
+        // 检查省份通行的黑名单
+        if (record.has("province")) {
+            String province = record.get("province").asText();
+            if (client.getProvinceBlacklist().stream().anyMatch(province::contains)) {
+                log.info("非法省份ip {} {}", ip, record);
+                return false;
             }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String ip = "178.173.225.155";
+        try (AWReader awReader = new AWReader(file)) {
+            InetAddress address = InetAddress.getByName(ip);
+            JsonNode record = awReader.get(address);
+            System.out.println(record);
         } catch (IpTypeException | IOException e) {
             log.error(e.getMessage(), e);
         }
-        return true;
     }
 
 }
