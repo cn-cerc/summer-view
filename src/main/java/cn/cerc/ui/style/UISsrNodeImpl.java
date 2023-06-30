050cf6c2f18cc52bb8cf26e79a85580889ea0802
张弓 <sz9214e@qq.com>
2023-06-30 08:03:02 +0800

    Update UISsrNodeImpl.java

--- src/main/java/cn/cerc/ui/style/UISsrNodeImpl.java
+++ src/main/java/cn/cerc/ui/style/UISsrNodeImpl.java
@@ -1,13 +1,25 @@
 package cn.cerc.ui.style;
 
 public interface UISsrNodeImpl {
-    
+
     void setTemplate(UITemplateImpl template);
-    
-    String getText();
-    
-    String getValue();
-    
+
+    /**
+     * 
+     * @return 原始字符串
+     */
     String getSourceText();
+
+    /**
+     * 
+     * @return 预处理过的字符串
+     */
+    String getText();
+
+    /**
+     * 
+     * @return 返回处理过的字符串
+     */
+    String getValue();
 
 }
