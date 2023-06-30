a12e3178327c4b0ac457fb9672d0e2d929734c76
梁志祥 <321337450@qq.com>
2023-06-30 21:53:49 +0800

    框架修改

--- src/main/java/cn/cerc/ui/style/SsrValueNode.java
+++ src/main/java/cn/cerc/ui/style/SsrValueNode.java
@@ -23,11 +23,12 @@ public class SsrValueNode implements SsrNodeImpl {
     public String getSourceText() {
         return "${" + this.text + "}";
     }
-    
+
     @Override
     public String getValue() {
         var field = this.getField();
         var params = this.getTemplate().getParams();
+        var map = this.getTemplate().getMap();
         var dataRow = this.getTemplate().getDataRow();
         if (Utils.isNumeric(field)) {
             if (params != null) {
@@ -41,8 +42,10 @@ public class SsrValueNode implements SsrNodeImpl {
             } else {
                 return this.getSourceText();
             }
-        } else if (dataRow != null) {
-            if (dataRow.exists(field)) {
+        } else if (map != null || dataRow != null) {
+            if (map != null && map.containsKey(field))
+                return map.get(field);
+            else if (dataRow != null && dataRow.exists(field)) {
                 return dataRow.getText(field);
             } else {
                 log.error("not find field: {}", field);
