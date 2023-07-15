ba37791333c35e0d5d709b322b6ffa80462cfc67
胡红昌 <2220393782@qq.com>
2023-07-15 11:07:46 +0000

    !252 读取文件时增加id不为空判断

--- src/main/java/cn/cerc/ui/style/SsrUtils.java
+++ src/main/java/cn/cerc/ui/style/SsrUtils.java
@@ -8,11 +8,16 @@ import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cn.cerc.db.core.Utils;
+
 public class SsrUtils {
     private static final Logger log = LoggerFactory.getLogger(SsrUtils.class);
 
     public static String getTempateFileText(Class<?> class1, String id) {
-        var fileName = class1.getSimpleName() + "_" + id + ".html";
+        var fileName = class1.getSimpleName() + ".html";
+        if (!Utils.isEmpty(id))
+            fileName = class1.getSimpleName() + "_" + id + ".html";
+
         var file = class1.getResourceAsStream(fileName);
         var list = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
         String line;
