644403632e2ef729b625f76d8413f96d4314a8a4
李远 <460289628@qq.com>
2023-08-07 07:44:59 +0000

    !401 增加获取单个插件的方法

--- src/main/java/cn/cerc/ui/plugins/PluginsFactory.java
+++ src/main/java/cn/cerc/ui/plugins/PluginsFactory.java
@@ -3,6 +3,7 @@ package cn.cerc.ui.plugins;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.context.ApplicationContext;
 
@@ -10,6 +11,20 @@ import cn.cerc.mis.core.Application;
 
 public class PluginsFactory {
 //    private static final Logger log = LoggerFactory.getLogger(PluginsFactory.class);
+
+    public static <T extends PluginsImpl> Optional<T> getPluginsOne(Object owner, Class<T> requiredType) {
+        var list = getPluginsList(owner, requiredType);
+        if (list.isEmpty())
+            return Optional.empty();
+        else if (list.size() > 1) {
+            List<String> items = new ArrayList<>();
+            for (var plugins : list)
+                items.add(plugins.getClass().getSimpleName());
+            throw new RuntimeException(String.format("接口 %s 出现重复的实现类：%s", requiredType.getClass().getSimpleName(),
+                    String.join(",", items)));
+        }
+        return Optional.of(list.get(0));
+    }
 
     public static <T extends PluginsImpl> List<T> getPluginsList(Object owner, Class<T> requiredType) {
         Objects.requireNonNull(owner);
