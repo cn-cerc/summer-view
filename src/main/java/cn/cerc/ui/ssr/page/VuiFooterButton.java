0fd8d733d124cf39d668a2e91b904ea0c4d8bc16
胡杰 <h2syj.zzzz@gmail.com>
2023-08-28 12:37:29 +0000

    !483 添加底部按钮组件，VuiForm组件库添加提交按钮

---
+++ src/main/java/cn/cerc/ui/ssr/page/VuiFooterButton.java
@@ -0,0 +1,60 @@
+package cn.cerc.ui.ssr.page;
+
+import java.util.Optional;
+
+import javax.persistence.Column;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import cn.cerc.db.core.Utils;
+import cn.cerc.ui.ssr.core.SsrBlock;
+import cn.cerc.ui.ssr.core.VuiComponent;
+import cn.cerc.ui.ssr.editor.SsrMessage;
+import cn.cerc.ui.ssr.form.ISupplierDataRow;
+import cn.cerc.ui.ssr.source.Binder;
+import cn.cerc.ui.vcl.ext.UIBottom;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Description("页面底部按钮")
+public class VuiFooterButton extends VuiComponent implements ISupportCanvas {
+    @Column
+    String text = "button";
+    @Column
+    String href = "";
+    @Column
+    String target = "";
+    @Column
+    Binder<ISupplierDataRow> dataRow = new Binder<>(this, ISupplierDataRow.class);
+
+    @Override
+    public String getIdPrefix() {
+        return "button";
+    }
+
+    @Override
+    public void onMessage(Object sender, int msgType, Object msgData, String targetId) {
+        switch (msgType) {
+        case SsrMessage.InitFooter:
+            if (msgData instanceof IFooter footer) {
+                UIBottom button = footer.addButton();
+                button.setCaption(text);
+
+                SsrBlock url = new SsrBlock(this.href);
+                Optional<ISupplierDataRow> optDataRow = this.dataRow.target();
+                if (optDataRow.isPresent())
+                    url.dataRow(optDataRow.get().dataRow());
+
+                button.setUrl(url.html());
+                if (!Utils.isEmpty(target))
+                    button.setTarget(target);
+                button.setId(this.getId());
+            }
+            break;
+        }
+    }
+
+}
