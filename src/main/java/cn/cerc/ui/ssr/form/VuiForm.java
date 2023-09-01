c21dc098373de467dab0b3b97fb4ab78690b42bc
胡杰 <h2syj.zzzz@gmail.com>
2023-09-01 07:10:28 +0000

    !511 VuiForm以及VuiGrid添加模板支持

--- src/main/java/cn/cerc/ui/ssr/form/VuiForm.java
+++ src/main/java/cn/cerc/ui/ssr/form/VuiForm.java
@@ -49,6 +49,7 @@ import cn.cerc.ui.ssr.editor.ISsrBoard;
 import cn.cerc.ui.ssr.editor.SsrMessage;
 import cn.cerc.ui.ssr.page.ISupportCanvas;
 import cn.cerc.ui.ssr.page.IVuiEnvironment;
+import cn.cerc.ui.ssr.page.VuiEnvironment;
 import cn.cerc.ui.ssr.source.Binder;
 import cn.cerc.ui.ssr.source.Binders;
 import cn.cerc.ui.ssr.source.IBinders;
@@ -81,6 +82,8 @@ public class VuiForm extends VuiContainer<ISupportForm>
     Binder<ISupplierDataRow> dataRow = new Binder<>(this, ISupplierDataRow.class);
     @Column
     AlignEnum align = AlignEnum.None;
+    @Column
+    boolean enableConfig = true;
     private IHandle handle;
 
     public VuiForm() {
@@ -564,6 +567,14 @@ public class VuiForm extends VuiContainer<ISupportForm>
                     buffer.post();
                 binders.findOwner(VuiDataService.class)
                         .ifPresent(service -> service.onMessage(this, SsrMessage.InitContent, null, null));
+                if (enableConfig) {
+                    if (canvas().environment() instanceof VuiEnvironment environment) {
+                        String pageCode = environment.getPageCode();
+                        this.templateId(pageCode);
+                        this.columns.clear();
+                        this.loadConfig(handle);
+                    }
+                }
             } else {
                 log.error("request 为空，无法执行");
             }
--- src/main/java/cn/cerc/ui/ssr/grid/VuiGrid.java
+++ src/main/java/cn/cerc/ui/ssr/grid/VuiGrid.java
@@ -44,6 +44,7 @@ import cn.cerc.ui.ssr.editor.ISsrBoard;
 import cn.cerc.ui.ssr.editor.SsrMessage;
 import cn.cerc.ui.ssr.page.ISupportCanvas;
 import cn.cerc.ui.ssr.page.IVuiEnvironment;
+import cn.cerc.ui.ssr.page.VuiEnvironment;
 import cn.cerc.ui.ssr.source.Binder;
 import cn.cerc.ui.ssr.source.Binders;
 import cn.cerc.ui.ssr.source.IBinders;
@@ -82,6 +83,9 @@ public class VuiGrid extends VuiContainer<ISupportGrid> implements ISsrBoard, IG
     String emptyText = "";
     @Column
     Binder<VuiDataService> dataSet = new Binder<>(this, VuiDataService.class);
+    @Column
+    private boolean enableConfig = true;
+    private IHandle handle;
 
     public VuiGrid() {
         super();
@@ -605,6 +609,10 @@ public class VuiGrid extends VuiContainer<ISupportGrid> implements ISsrBoard, IG
     @Override
     public void onMessage(Object sender, int msgType, Object msgData, String targetId) {
         switch (msgType) {
+        case SsrMessage.InitHandle:
+            if (msgData instanceof IHandle handle)
+                this.handle = handle;
+            break;
         case SsrMessage.InitRequest:
             if (msgData instanceof HttpServletRequest request)
                 this.request = request;
@@ -624,6 +632,16 @@ public class VuiGrid extends VuiContainer<ISupportGrid> implements ISsrBoard, IG
                 this.dataSet(bean.get().dataSet());
             else
                 log.warn("{} 绑定的数据源 {} 找不到", this.getId(), this.dataSet.targetId());
+            break;
+        case SsrMessage.InitContent:
+            if (enableConfig) {
+                if (canvas().environment() instanceof VuiEnvironment environment) {
+                    String pageCode = environment.getPageCode();
+                    this.templateId(pageCode + "_grid");
+                    this.columns.clear();
+                    this.loadConfig(handle);
+                }
+            }
             break;
         }
     }
