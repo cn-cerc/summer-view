32d2c847a0841afe026e3ccb352608170fc0bb7a
胡杰 <h2syj.zzzz@gmail.com>
2023-09-13 06:46:36 +0000

    !561 完善 VuiReturnPage 组件支持绑定数据源进行传参

--- src/main/java/cn/cerc/ui/ssr/page/VuiReturnPage.java
+++ src/main/java/cn/cerc/ui/ssr/page/VuiReturnPage.java
@@ -11,6 +11,8 @@ import cn.cerc.db.core.Utils;
 import cn.cerc.ui.ssr.core.SsrBlock;
 import cn.cerc.ui.ssr.core.VuiComponent;
 import cn.cerc.ui.ssr.editor.SsrMessage;
+import cn.cerc.ui.ssr.source.Binder;
+import cn.cerc.ui.ssr.source.ISupplierDataRow;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -21,16 +23,30 @@ public class VuiReturnPage extends VuiComponent implements ISupportCanvas {
     String href = "";
     @Column
     String title = "";
+    @Column
+    Binder<ISupplierDataRow> binder = new Binder<>(this, ISupplierDataRow.class);
+    private IHeader header;
 
     @Override
     public void onMessage(Object sender, int msgType, Object msgData, String targetId) {
         switch (msgType) {
+        case SsrMessage.InitBinder:
+            binder.init();
+            break;
         case SsrMessage.InitHeader:
             if (Utils.isEmpty(this.href) || Utils.isEmpty(this.title))
                 return;
-            if (msgData instanceof IHeader impl) {
-                impl.addLeftMenu(this.href, this.title);
+            if (msgData instanceof IHeader impl)
+                this.header = impl;
+            break;
+        case SsrMessage.InitContent:
+            String url = this.href;
+            if (binder.target().isPresent()) {
+                SsrBlock block = new SsrBlock(this.href);
+                block.dataRow(binder.target().get().dataRow());
+                url = block.html();
             }
+            header.addLeftMenu(url, this.title);
             break;
         }
     }
